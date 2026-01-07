6e5545fc55cc2be241c45ad9ad2618dd4857c069
tjkam <tjkamlesh@gmail.com>
2026-01-07 17:55:52 +0530

    removed opencv and numpy from requirements

--- version_check.py
+++ version_check.py
@@ -1,4 +1,4 @@
-import numpy, spacy, cv2
+import numpy, moviepy, cv2
 print("numpy:", numpy.__version__)
-print("spacy:", spacy.__version__)
+print("spacy:", moviepy.__version__)
 print("opencv:", cv2.__version__)
